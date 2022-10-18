67e2663d003f774d8f8819058da3faa128c15c88
nian_liu <nian_liu@fzzixun.com>
2022-10-18 16:49:15 +0800

    测试代码回退

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class MyApp extends StatelessWidget {
       theme: theme ?? provider.getTheme(),
       darkTheme: provider.getTheme(isDarkMode: true),
       themeMode: provider.getThemeMode(),
-      home:const SettingPage(),
+      home:home ?? const SplashPage(),
       onGenerateRoute: Routers.router.generator,
       localizationsDelegates: const [
         S.delegate,
