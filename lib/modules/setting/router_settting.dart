255e380953bebed06d636db2a6a9ab3e6070e96b
nian_liu <nian_liu@fzzixun.com>
2022-10-19 16:37:50 +0800

    用户信息修改页编码

--- lib/modules/setting/router_settting.dart
+++ lib/modules/setting/router_settting.dart
@@ -1,13 +1,17 @@
 import 'package:fluro/fluro.dart';
 import 'package:zpass/modules/setting/setting_page.dart';
+import 'package:zpass/modules/setting/user_info_setting.dart';
 import 'package:zpass/routers/i_router.dart';
 
 class RouterSetting extends IRouterProvider {
   static const String setting = '/setting';
+  static const String userInfoSetting = '/setting/userinfo';
 
   @override
   void initRouter(FluroRouter router) {
     router.define(setting,
         handler: Handler(handlerFunc: (_, __) => const SettingPage()));
+    router.define(userInfoSetting,
+        handler: Handler(handlerFunc: (_, __) => const UserInfoSettingPage()));
   }
 }
--- lib/modules/setting/setting_page.dart
+++ lib/modules/setting/setting_page.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zpass/modules/setting/general.dart';
+import 'package:zpass/modules/setting/router_settting.dart';
 import 'package:zpass/modules/setting/security.dart';
-import 'package:zpass/modules/setting/switch.dart';
 import 'package:zpass/modules/user/model/user_info_model.dart';
+import 'package:zpass/modules/user/user_provider.dart';
 import 'package:zpass/res/zpass_icons.dart';
-import 'package:zpass/widgets/list.dart';
+import 'package:zpass/routers/fluro_navigator.dart';
 import 'package:zpass/widgets/load_image.dart';
 
 class SettingPage extends StatefulWidget {
@@ -47,92 +47,101 @@ class _SettingPageState extends State<SettingPage> {
 
   @override
   Widget build(BuildContext context) {
-    UserInfoModel _userInfo = UserInfoModel();
+    UserInfoModel userInfo = UserProvider().getUserInfo();
     return Container(
       padding: const EdgeInsets.fromLTRB(16, 20.5, 16, 20.5),
       color: const Color.fromRGBO(247, 247, 247, 1),
       child: Column(
         children: [
           Container(
-            color: Colors.cyan,
-            alignment: Alignment.centerLeft,
-            child: const LoadAssetImage(
-              'signin/pass-on@2x',
-              width: 20,
-              height: 20,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(11.5, 19.5, 13, 19.5),
-            margin: const EdgeInsets.only(top: 20.5),
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(11))),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(50)),
-                  child: LoadImage(
-                    _userInfo.icon ?? '',
-                    width: 34,
-                    height: 34,
-                  ),
+              margin: const EdgeInsets.only(top: 18),
+              alignment: Alignment.centerLeft,
+              child: GestureDetector(
+                child: Icon(
+                  ZPassIcons.icClose,
+                  color: _rightColor,
+                  size: 18.5,
                 ),
-                Container(
-                  margin: const EdgeInsets.only(left: 12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Material(
-                            child: Text(
-                              _userInfo.name ?? '',
-                              style: const TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Material(
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 5),
-                              padding: const EdgeInsets.fromLTRB(7, 3, 7, 3),
-                              decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(4)),
-                                  color:
-                                      const Color.fromRGBO(73, 84, 255, 0.0800),
-                                  border: Border.all(
-                                      width: 1,
-                                      color: const Color.fromRGBO(
-                                          73, 84, 255, 0.3000))),
+                onTap: () {
+                  NavigatorUtils.goBack(context);
+                },
+              )),
+          GestureDetector(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(11.5, 19.5, 13, 19.5),
+              margin: const EdgeInsets.only(top: 20.5),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(11))),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(50)),
+                    child: LoadImage(
+                      userInfo.icon ?? '',
+                      width: 34,
+                      height: 34,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Material(
                               child: Text(
-                                _userInfo.type ?? '',
+                                userInfo.name ?? 'ZPASS',
                                 style: const TextStyle(
-                                    fontSize: 10,
-                                    color: Color.fromRGBO(73, 84, 255, 1)),
+                                    fontSize: 15, fontWeight: FontWeight.bold),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Material(
-                        child: Text(
-                          _userInfo.email ?? '',
-                          style: const TextStyle(
-                              color: Color.fromRGBO(149, 155, 167, 1)),
+                            Material(
+                              child: Container(
+                                margin: const EdgeInsets.only(left: 5),
+                                padding: const EdgeInsets.fromLTRB(7, 3, 7, 3),
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(4)),
+                                    color:
+                                    const Color.fromRGBO(73, 84, 255, 0.0800),
+                                    border: Border.all(
+                                        width: 1,
+                                        color: const Color.fromRGBO(
+                                            73, 84, 255, 0.3000))),
+                                child: Text(
+                                  userInfo.type ?? 'zpass',
+                                  style: const TextStyle(
+                                      fontSize: 10,
+                                      color: Color.fromRGBO(73, 84, 255, 1)),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        Material(
+                          child: Text(
+                            userInfo.email ?? '',
+                            style: const TextStyle(
+                                color: Color.fromRGBO(149, 155, 167, 1)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const Spacer(),
-                Icon(
-                  ZPassIcons.icArrowRight,
-                  color: _rightColor,
-                  size: 10,
-                )
-              ],
+                  const Spacer(),
+                  Icon(
+                    ZPassIcons.icArrowRight,
+                    color: _rightColor,
+                    size: 10,
+                  )
+                ],
+              ),
             ),
+            onTap: (){
+              NavigatorUtils.push(context, RouterSetting.userInfoSetting);
+            },
           ),
           GeneralWidget(rightStyle: _rightTextStyle, rightColor: _rightColor),
           SecurityWidget(rightStyle: _rightTextStyle, rightColor: _rightColor)
