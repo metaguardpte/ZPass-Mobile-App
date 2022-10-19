62bf61faa0c0f905d47ed0b8d6d5a95133f7ec3e
nian_liu <nian_liu@fzzixun.com>
2022-10-19 15:19:12 +0800

    login增加返回按钮

--- lib/modules/user/signin/bg.dart
+++ lib/modules/user/signin/bg.dart
@@ -1,12 +1,17 @@
+import 'package:flkv/flkv.dart';
 import 'package:flutter/material.dart';
+import 'package:zpass/res/zpass_icons.dart';
+import 'package:zpass/routers/fluro_navigator.dart';
+
+import 'package:zpass/routers/routers.dart';
 
 import '../../../widgets/load_image.dart';
 
-class BackWithLogo extends StatelessWidget{
-  const BackWithLogo({Key? key}):super(key:key);
+class BackWithLogo extends StatelessWidget {
+  const BackWithLogo({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.topCenter,
       children: [
@@ -20,6 +25,17 @@ class BackWithLogo extends StatelessWidget{
               width: double.infinity,
               fit: BoxFit.fitWidth,
             )),
+        Positioned(
+          left: 10,
+          top: 30,
+          child: IconButton(
+            icon:const Icon(ZPassIcons.icNavBack, color: Colors.white, size: 16),
+            padding:const EdgeInsets.all(0),
+            onPressed: () {
+              NavigatorUtils.push(
+                  context, Routers.loginOrNew, clearStack: true);
+            },),
+        ),
         Container(
           margin: const EdgeInsets.fromLTRB(0, 92.5, 0, 0),
           child: const LoadAssetImage(
