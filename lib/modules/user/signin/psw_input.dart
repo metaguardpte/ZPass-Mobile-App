cc40ae34d6067f40e2364319c3ffb12b22df81b7
nian_liu <nian_liu@fzzixun.com>
2022-10-20 16:37:38 +0800

    登录输入框调整

--- lib/modules/user/signin/psw_input.dart
+++ lib/modules/user/signin/psw_input.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:zpass/generated/l10n.dart';
+import 'package:zpass/modules/user/signin/zpass_input.dart';
+import 'package:zpass/res/zpass_icons.dart';
 import 'package:zpass/util/callback_funcation.dart';
 
 import '../../../widgets/load_image.dart';
@@ -28,37 +30,38 @@ class _PswInputState extends State<PswInput> {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return ZPassTextFieldWidget(
       onChanged: (value) {
         widget.onChange?.call(value);
       },
       obscureText: !_showPasword,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.zero,
-          suffixIcon: IconButton(
-              alignment: Alignment.centerRight,
-              onPressed: switchPassword,
-              icon: _showPasword
-                  ? const LoadAssetImage(
-                      'signin/pass-on@2x',
-                      width: 20,
-                      height: 20,
-                    )
-                  : const LoadAssetImage(
-                      'signin/pass-off@2x',
-                      width: 20,
-                      height: 20,
-                    )),
-          icon: const LoadAssetImage(
-            'signin/LockKey@2x',
-            width: 20,
-            height: 20,
-          ),
-          hintText: S.current.password,
-          hintStyle: const TextStyle(color: Color.fromRGBO(147, 151, 157, 1)),
-          border: const OutlineInputBorder(
-            borderSide: BorderSide.none,
-          )),
+      hintText: S.current.password,
+      icon: const LoadAssetImage(
+        'signin/LockKey@2x',
+        width: 20,
+        height: 20,
+      ),
+      suffixIcon: IconButton(
+          alignment: Alignment.centerRight,
+          onPressed: switchPassword,
+          icon: _showPasword
+              ? const Icon(ZPassIcons.icNoSecret,color: Color.fromRGBO(174, 183, 197, 1),size: 18)
+              : const Icon(ZPassIcons.icSecret,color: Color.fromRGBO(174, 183, 197, 1),size: 18)
+      ),
     );
+
+    //   TextField(
+    //
+    //
+    //   decoration: InputDecoration(
+    //       contentPadding: EdgeInsets.zero,
+    //       suffixIcon:
+    //       icon: const LoadAssetImage(
+    //         'signin/LockKey@2x',
+    //         width: 20,
+    //         height: 20,
+    //       ),
+
+    // );
   }
 }
