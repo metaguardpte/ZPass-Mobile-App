da38bb6e7af625471099faab910b7b8f89759237
guoquanlu <quanlu_guo@fzzixun.com>
2022-10-26 11:14:09 +0800

    Integrated local_auth library

--- android/app/src/main/kotlin/com/zpassapp/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/zpassapp/mobile/MainActivity.kt
@@ -3,10 +3,10 @@ package com.zpassapp.mobile
 import android.os.Bundle
 import com.zpassapp.mobile.rpc.RpcManager
 import com.zpassapp.mobile.rpc.SystemNavigatorProxy
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SystemNavigatorProxy.attachActivity(this)
