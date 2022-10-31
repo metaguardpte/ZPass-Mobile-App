0f4c5bc6207de53a80794a943ece0e80b03007c0
zhuming_xie <zhuming_xie@fzzixun.com>
2022-10-31 11:09:36 +0800

    support compress file list

--- lib/modules/sync/file_transfer/base_file_transfer.dart
+++ lib/modules/sync/file_transfer/base_file_transfer.dart
@@ -7,28 +7,37 @@ import 'package:zpass/plugin_bridge/p7zip/p7zip.dart';
 
 abstract class BaseFileTransferManager {
 
-  final String defaultZipFileName = "zpass.7z";
+  final String defaultZipFileName = "db.7z";
+  final String separator = Platform.pathSeparator;
 
-  Future<String> doDownload();
+  Future<String> doDownload(String userId);
 
-  Future doUpload(String source, {String dest=''});
+  Future doUpload(String source, String userId);
 
   ///zip file path in dedicated storage
-  Future<String> download() async {
-    String localZipFile = await doDownload();
+  Future<String> download(String userId) async {
+    String localZipFile = await doDownload(userId);
     String? decompressToPath = await P7zip.decompressZipToPath(fromZip: localZipFile);
     return decompressToPath!;
   }
 
   ///source: directory that contains files to be archived
-  Future upload(String source) async {
-    final tempDir = await getTemporaryDirectory();
-    String tempPath = "$tempDir/${getUniqueDir()}";
-    copyDir(source, tempPath);
+  Future upload(String source, String userId) async {
+    var tempDir = await getTemporaryDirectory();
+    String uniquePath = '${tempDir.path}$separator${getUniqueDir()}$separator';
+    copyDir(source, uniquePath);
 
-    String zipFilePath = "$tempPath/$defaultZipFileName";
-    await P7zip.compressPathToZip(fromPath: tempPath, toZip: zipFilePath);
-    doUpload(zipFilePath);
+    List<String> toBeArchivedFiles = <String>[];
+    List<FileSystemEntity> tempFiles = Directory(uniquePath).listSync();
+    for(FileSystemEntity f in tempFiles) {
+      if (FileSystemEntity.isDirectorySync(f.path)) {
+        continue;
+      }
+      toBeArchivedFiles.add(f.path);
+    }
+    String zipFilePath = '$uniquePath$defaultZipFileName';
+    await P7zip.compressFileListToZip(files: toBeArchivedFiles, toZip: zipFilePath);
+    await doUpload(zipFilePath, userId);
   }
 
   String getUniqueDir(){
@@ -40,8 +49,16 @@ abstract class BaseFileTransferManager {
     newDir.createSync(recursive: true);
 
     List<FileSystemEntity> fileList = Directory(fromDir).listSync();
+
     for(FileSystemEntity f in fileList){
-      File("$toDir/${f.path}").writeAsBytesSync(File(f.path).readAsBytesSync());
+      if (FileSystemEntity.isDirectorySync(f.path)) {
+        continue;
+      }
+      String path = f.path;
+      var lastSeparator = path.lastIndexOf(separator);
+      var fileName = path.substring(lastSeparator + 1, path.length);
+
+      File("$toDir$separator$fileName").writeAsBytesSync(File(path).readAsBytesSync());
     }
   }
 
